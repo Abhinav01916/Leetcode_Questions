f456f459dcf3e240ca9cf4b5b12346d0e06af5d9
Abhinav Anand <abhinavanand3162@gmail.com>
2024-10-14 20:19:37 +0530

    Create 7.Reverse Integer.cpp

---
+++ LC_Random/7.Reverse Integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+      int answer= 0;
+      while(x!=0)
+      {  
+       int digit=x%10;
+
+       if((answer>INT_MAX/10) || (answer<INT_MIN/10)) // edge case 
+       {
+        return 0;
+       }
+       answer=(10*answer)+digit;
+          x=x/10;
+      }
+    
+      return answer;
+       
+
+    }
+    
+};
