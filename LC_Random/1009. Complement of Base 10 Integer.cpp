b7ac52f97a68173a873b1eebd08f3d33f30132c0
Abhinav Anand <abhinavanand3162@gmail.com>
2024-10-19 22:44:13 +0530

    Create 1009. Complement of Base 10 Integer.cpp

---
+++ LC_Random/1009. Complement of Base 10 Integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=n;
+        int mask = 0;
+        // creating mask for ~n
+
+        if (n==0)
+        {
+            return 1; //  edge case not covered in the logic
+        }
+        while(m!=0){
+         mask = (mask << 1) | 1;
+         m = m >>1;
+        }
+        int answer= (~n) & mask ;
+        return answer;
+    }
+
+
+};
