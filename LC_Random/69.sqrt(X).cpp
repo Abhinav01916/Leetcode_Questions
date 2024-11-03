4fb0397726a8ba6397fc4bfe8401b568292c51d0
Abhinav Anand <abhinavanand3162@gmail.com>
2024-11-03 21:38:41 +0530

    Create 69.sqrt(X).cpp

---
+++ LC_Random/69.sqrt(X).cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        // implementing the concept of binary search here 
+            if(x==0){
+                return 0;
+            }
+            int answer=1;
+            int high=x;
+            int low=1;
+            long mid;
+            while(low<=high )
+            {
+                 mid=low+(high-low)/2;
+                if((mid*mid)<=x)
+                {
+                    answer=mid;
+                    low=mid+1;
+                }
+                else 
+                {
+                    high=mid-1;
+                }
+            }
+            return answer;
+
+       
+    }
+};
