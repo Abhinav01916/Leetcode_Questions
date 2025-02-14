354491519f242b25e50fc4ac82e01fff650265bc
Abhinav Anand <abhinavanand3162@gmail.com>
2025-02-14 23:00:16 +0530

    Update 231. Power of Two.cpp

--- LC_Random/231. Power of Two.cpp
+++ LC_Random/231. Power of Two.cpp
@@ -3,7 +3,7 @@ class  Solution
 public:
     bool isPowerOfTwo(int n) {
         int ans = 1;
-        for(int i=0;i<=30;i++)
+        for(int i=0; i<=30; i++)
         {
             if(ans==n)
             {
