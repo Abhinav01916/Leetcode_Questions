f7d1e247452a2af46432920e42bc4ccc9c522a34
Abhinav Anand <abhinavanand3162@gmail.com>
2025-04-28 20:54:01 +0530

    Create 2798.Number of Employees Who Met the Target.cpp

---
+++ LC_Random/2798.Number of Employees Who Met the Target.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int sum =0;
+        for(int i=0;i<hours.size();i++){
+            if(hours[i]>=target){
+                sum+=1;
+            }
+        }
+        return sum;
+    }
+};
